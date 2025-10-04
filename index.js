a1a6814b4669530bd7159b0a9cf37072b9517cdc
Vaibhav Tiwari <93003178+vaibhavtiwari0112@users.noreply.github.com>
2025-10-04 22:58:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ const cors = require("cors");
 const paymentRoutes = require("./routes/payments");
 
 const app = express();
+app.use((req, res, next) => {
+  console.log("➡️ Incoming request:", req.method, req.url);
+  next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
